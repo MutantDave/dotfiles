69687c68586c01436441bf18f00a16fa9bedf031
FancyRatsDave <dave@fancyrats.io>
2022-07-06 09:10:31 -0500

    Update nord

--- install-nord.sh
+++ install-nord.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-git clone git@github.com:arcticicestudio/nord-dircolors.git ~/.nord-dircolors
+git clone https://github.com/arcticicestudio/nord-dircolors.git ~/.nord-dircolors
 cp -rf ~/.nord-dircolors/src/dir_colors ~/.dir_colors
 rm -rf ~/.nord-dircolors
 
