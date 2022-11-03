b5f1dbd412e3ebb0e3981df2012be0616c57dcbb
FancyRatsDave <dave@fancyrats.io>
2022-11-03 15:10:31 -0500

    Update to remove deprecated call

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -7,7 +7,7 @@ if (not saga_status) then return end
 
 local nnoremap = require("user.keymap").nnoremap
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(
+local capabilities = require('cmp_nvim_lsp').default_capabilities(
   vim.lsp.protocol.make_client_capabilities()
 )
 
