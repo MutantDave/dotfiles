e605097933730b9acbf9075f9dbed5144c150089
Dave Henke <dave@fancyrats.io>
2023-03-27 11:02:14 -0500

    Support AWS cloudformation yaml

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -27,28 +27,30 @@ lsp.configure('solidity', {
   }
 })
 
-lsp.configure('yaml', {
+lsp.configure('yamlls', {
   settings = {
-    customTags = {
-      "!fn",
-      "!And",
-      "!If",
-      "!Not",
-      "!Equals",
-      "!Or",
-      "!FindInMap sequence",
-      "!Base64",
-      "!Cidr",
-      "!Ref",
-      "!Ref Scalar",
-      "!Sub",
-      "!GetAtt",
-      "!GetAZs",
-      "!ImportValue",
-      "!Select",
-      "!Split",
-      "!Join sequence"
-    },
+    yaml = {
+      customTags = {
+        "!fn",
+        "!And",
+        "!If",
+        "!Not",
+        "!Equals",
+        "!Or",
+        "!FindInMap sequence",
+        "!Base64",
+        "!Cidr",
+        "!Ref",
+        "!Ref Scalar",
+        "!Sub",
+        "!GetAtt",
+        "!GetAZs",
+        "!ImportValue",
+        "!Select",
+        "!Split",
+        "!Join sequence"
+      },
+    }
   }
 })
 
