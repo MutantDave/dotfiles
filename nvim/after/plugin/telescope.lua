ec276c1952233162866055399e158b9f6cb40570
Dave Henke <dave@fancyrats.io>
2023-04-11 17:42:53 -0400

    Update telescope binding

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,7 @@
+local telescope = require('telescope')
+telescope.setup({
+
+})
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>o', builtin.find_files, {})
-vim.keymap.set('n', '<leader>p', builtin.git_files, {})
-vim.keymap.set('n', '<leader>s', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+vim.keymap.set('n', '<leader>p', builtin.find_files, {})
+vim.keymap.set('n', '<leader>s', builtin.live_grep, {})
