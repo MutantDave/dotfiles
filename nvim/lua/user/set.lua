a7a97eda577be58ac6f3700c87d4452a293ffa5f
Dave Henke <dave@fancyrats.io>
2023-11-18 09:17:18 -0600

    Highlight yank

--- nvim/lua/user/set.lua
+++ nvim/lua/user/set.lua
@@ -30,3 +30,11 @@ vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
+
+-- highlight yanked text for 200ms using the "Visual" highlight group
+vim.cmd[[
+augroup highlight_yank
+autocmd!
+au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
+augroup END
+]]
