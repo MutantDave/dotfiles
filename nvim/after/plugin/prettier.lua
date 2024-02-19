9729a093e12b3e11f132fea700626564b7d8b61a
Dave Henke <dave@fancyrats.io>
2024-02-18 19:26:21 -0600

    Updates from work

--- nvim/after/plugin/prettier.lua
+++ nvim/after/plugin/prettier.lua
@@ -1,4 +1,4 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = {"*.js","*.jsx","*.mjs","*.ts","*.tsx","*.css","*.less","*.scss","*.json","*.graphql","*.vue","*.svelte","*.yaml","*.html","*.sol","*.cjs","*.prisma"},
+  pattern = {"*.js","*.jsx","*.mjs","*.ts","*.tsx","*.css","*.less","*.scss","*.json","*.graphql","*.vue","*.svelte","*.html","*.sol","*.cjs","*.prisma"},
    command = "Prettier"
 })
