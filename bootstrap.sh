2f2632e77ec85c4f80380cd45e7e92c821b6215a
Dave Henke <dave.p.henke@gmail.com>
2024-02-18 20:40:59 -0600

    Stow it up

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+script_dir=$(cd $(dirname $0)/scripts && pwd)
+
+function install {
+  sh $script_dir/install/$1.sh
+}
+
+install homebrew
+brew bundle
+
+install tpm
+~/.tmux/plugins/tpm/bin/install_plugins
+
+install omz
+install rust
+install npm
+install foundry
+
+
+
