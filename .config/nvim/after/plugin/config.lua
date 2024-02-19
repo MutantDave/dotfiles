fe6e13f6192b9e4eb677137f28c8de0484296105
Dave Henke <dave.p.henke@gmail.com>
2024-02-18 21:27:03 -0600

    Telescope background transparent

--- .config/nvim/after/plugin/config.lua
+++ .config/nvim/after/plugin/config.lua
@@ -1,11 +1,32 @@
 vim.g.vim_json_conceal = 0
 vim.g.markdown_syntax_conceal = 0
 
-vim.cmd [[colorscheme tokyonight]]
+vim.cmd [[colorscheme tokyonight-night]]
 vim.g.NERDTreeMinimalMenu = 1
 vim.g.NERDTreeShowHidden = 1
 vim.g.NERDTreeWinSize = 60
 vim.g.NERDTreeIgnore = {'.DS_Store'}
 
 require'colorizer'.setup({})
+require'transparent'.setup({
+  extra_groups = { -- table/string: additional groups that should be cleared
+  "TelescopePromptTitle",
+  "TelescopePromptPrefix",
+  "TelescopePromptCounter",
+  "TelescopePromptNormal",
+  "TelescopePromptBorder",
+
+  "TelescopeMatching",
+  "TelescopeSelection",
+
+  "TelescopePreviewTitle",
+  "TelescopePreviewNormal",
+  "TelescopePreviewBorder",
+
+  "TelescopeResultsTitle",
+  "TelescopeResultsNormal",
+  "TelescopeResultsBorder"
+  },
+  exclude_groups = {}, -- table: groups you don't want to clear
+})
 vim.cmd [[:TransparentEnable]]
