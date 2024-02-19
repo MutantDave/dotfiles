7fd10ba6c16cdd606ba5e5a80c34f7881a41d4c1
Dave Henke <dave.p.henke@gmail.com>
2024-02-18 22:27:37 -0600

    telescope a bit better

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -3,8 +3,13 @@ telescope.setup({
   defaults = {
     path_display = { 'shorten' },
   },
+  pickers = {
+    find_files = {
+      hidden = true
+    }
+  }
 })
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>p', builtin.git_files, {})
+vim.keymap.set('n', '<leader>p', builtin.find_files, {})
 vim.keymap.set('n', '<leader>s', builtin.live_grep, {})
