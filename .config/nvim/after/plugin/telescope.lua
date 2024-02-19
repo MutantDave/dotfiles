117646ddefb4bab6c730a245017a40f334a736bd
Dave Henke <dave.p.henke@gmail.com>
2024-02-18 21:31:09 -0600

    Use git files for telescope search

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -4,6 +4,7 @@ telescope.setup({
     path_display = { 'shorten' },
   },
 })
+
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>p', builtin.find_files, {})
+vim.keymap.set('n', '<leader>p', builtin.git_files, {})
 vim.keymap.set('n', '<leader>s', builtin.live_grep, {})
