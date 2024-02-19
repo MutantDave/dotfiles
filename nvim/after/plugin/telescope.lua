3e2abe298d177ddfb0a846326974b05cc10b4315
Dave Henke <dave@fancyrats.io>
2024-02-18 19:20:34 -0600

    Update from work repo

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,8 @@
 local telescope = require('telescope')
 telescope.setup({
-
+  defaults = {
+    path_display = { 'shorten' },
+  },
 })
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>p', builtin.find_files, {})
