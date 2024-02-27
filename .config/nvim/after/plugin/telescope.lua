6442a223f87d72d2a77f2f9ac8a3980780be9337
Dave Henke <dave.p.henke@gmail.com>
2024-02-27 08:33:42 -0600

    Smarter telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -6,5 +6,29 @@ telescope.setup({
 })
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>p', builtin.git_files, {})
+-- telescope-config.lua
+local M = {}
+
+-- We cache the results of "git rev-parse"
+-- Process creation is expensive in Windows, so this reduces latency
+local is_inside_work_tree = {}
+local builtin = require("telescope.builtin")
+
+M.project_files = function()
+  local opts = {} -- define here if you want to define something
+
+  local cwd = vim.fn.getcwd()
+  if is_inside_work_tree[cwd] == nil then
+    vim.fn.system("git rev-parse --is-inside-work-tree")
+    is_inside_work_tree[cwd] = vim.v.shell_error == 0
+  end
+
+  if is_inside_work_tree[cwd] then
+    builtin.git_files(opts)
+  else
+    builtin.find_files(opts)
+  end
+end
+
 vim.keymap.set('n', '<leader>s', builtin.live_grep, {})
+vim.keymap.set("n", "<leader>p", M.project_files, {})
